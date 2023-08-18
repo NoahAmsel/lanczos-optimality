9eb4cd84645209de0173ec0f88f882d53892b60e
Noah Amsel <noah.amsel@nyu.edu>
2023-08-18 00:34:30 -0400

    first step in fixing remez point exchange

--- matrix_functions/remez.py
+++ matrix_functions/remez.py
@@ -8,31 +8,58 @@ from .chebyshev import cheb_nodes, cheb_vandermonde, cheb_regression_errors
 from .utils import arange
 
 
-def discrete_remez_error(degree, f, points, max_iter=100, tol=1e-10):
+def extrema_indices(x, num):
+    # The indices must be returned in sorted order!
+    # Divide x into intervals where the sign is all the same
+    # Hack! deal with the places where x = 0. Perturb these so they fall strictly in one interval or the other
+    zero_ixs = np.nonzero(x == 0)[0]
+    eps = np.min(np.abs(x)) / 2
+    # alternate the signs of the perturbation in case there are consecutive zeros
+    x[zero_ixs] = eps * (-1) ** np.arange(len(zero_ixs))
+
+    # start_indices contains the index of the first element in each of these intervals
+    # x < 0 instead of np.sign(x) because if x is 0, we want a tie break
+    switch_ixs = list(np.nonzero(np.diff(np.sign(x)))[0] + 1)
+    start_ixs = [0] + switch_ixs
+    end_ixs = switch_ixs + [len(x)]
+    intervals = zip(start_ixs, end_ixs)
+    # find the max within each each interval
+    max_indices = [start_ix + np.argmax(np.abs(x[start_ix:end_ix])) for start_ix, end_ix in intervals]
+    # there may be more extreme points than we need
+    # keep the biggest ones, preserving alternating signs
+    while len(max_indices) > num:
+        if x[max_indices[0]] > x[max_indices[-1]]:
+            max_indices.pop()
+        else:
+            max_indices.pop(0)
+    assert len(max_indices) == num
+    return max_indices
+
+
+def discrete_remez_error(degree, f_points, points, max_iter=100, tol=1e-10):
+    # See page 301 of https://dl.acm.org/doi/pdf/10.1145/321281.321282
     points = np.array(points)
     a = points.min()
     b = points.max()
     def shift(x): return 2*(x-a)/(b-a) - 1  # send [a,b] to [-1, 1]
     def unshift(x): return x*(b-a)/2 + (a+b)/2  # send [-1, 1] to [a, b]
-    def ff(x): return f(unshift(x))
     points = shift(points)
-    f_points = ff(points)
 
     # initial guess: chebyshev l2 regression
-    my_errors = cheb_regression_errors(degree, f_points, points)
-    X = points[np.sort(np.hstack((0, find_peaks(my_errors)[0], find_peaks(-my_errors)[0], len(points)-1)))]
+    err_fun = cheb_regression_errors(degree, f_points, points)
+    extreme_ixs = extrema_indices(err_fun, degree + 2)
 
     # TODO: I added this block to handle the special case where the function is interpolated perfectly by Chebyshev
     # regression but I'm not sure it's the right thing
-    if (len(X) < degree + 2) and (np.max(np.abs(my_errors)) < tol):
-        return 0, np.max(np.abs(my_errors)), X
+    if (len(extreme_ixs) < degree + 2) and (np.max(np.abs(err_fun)) < tol):
+        return np.max(np.abs(err_fun))
 
     for _ in range(max_iter):
         V = np.hstack((
-            cheb_vandermonde(X, degree), ((-1)**arange(degree+2, dtype=points.dtype))[:, np.newaxis]
+            cheb_vandermonde(points[extreme_ixs], degree), ((-1)**arange(degree+2, dtype=points.dtype))[:, np.newaxis]
         ))
 
-        fX = ff(X)
+        fX = f_points[extreme_ixs]
 
         if points.dtype == np.dtype('O'):
             c = flamp.lu_solve(V, fX)
@@ -49,9 +76,7 @@ def discrete_remez_error(degree, f, points, max_iter=100, tol=1e-10):
         if np.abs(E-F) < tol:
             return F
 
-        extrema_indices = np.sort(np.hstack((0, find_peaks(err_fun)[0], find_peaks(-err_fun)[0], len(err_fun)-1)))
-        assert len(extrema_indices) == degree + 2
-        X = points[extrema_indices]
+        extreme_ixs = extrema_indices(err_fun, degree + 2)
 
     warnings.warn(f"Remez fail to converge after {max_iter} iterations.\nE={E}\nF={F}")
     return F
@@ -60,4 +85,4 @@ def discrete_remez_error(degree, f, points, max_iter=100, tol=1e-10):
 def remez_error(degree, f, domain=[-1, 1], max_iter=100, n_grid=2000, tol=1e-10):
     a, b = domain
     grid = cheb_nodes(n_grid-1, a=a, b=b, dtype=np.dtype('O'))
-    return discrete_remez_error(degree, f, grid, max_iter=max_iter, tol=tol)
+    return discrete_remez_error(degree, f(grid), grid, max_iter=max_iter, tol=tol)
--- tests/test_remez.py
+++ tests/test_remez.py
@@ -1,3 +1,4 @@
+import flamp
 import numpy as np
 
 import matrix_functions as mf
@@ -11,7 +12,39 @@ def test_discrete_remez_error():
 
     for degree in range(10):
         remez_error_continuous = mf.remez_error(degree, inv_sqrt, domain=(a, b), max_iter=10, tol=1e-16)
-        remez_error_discrete = mf.discrete_remez_error(degree, inv_sqrt, points, max_iter=10, tol=1e-16)
+        remez_error_discrete = mf.discrete_remez_error(degree, inv_sqrt(points), points, max_iter=10, tol=1e-16)
         regression_error = mf.norm(mf.chebyshev.cheb_regression_errors(degree, inv_sqrt(points), points), ord=np.inf)
         assert remez_error_continuous < regression_error
         assert remez_error_discrete < regression_error
+
+
+def test_oscillations():
+    points = mf.utils.linspace(-np.pi, np.pi, 101, dtype=np.dtype('O'))
+    remez_error_discrete = mf.discrete_remez_error(0, flamp.cos(points), points, max_iter=10, tol=1e-16)
+    assert np.abs(remez_error_discrete - 1) < flamp.gmpy2.mpfr('1e-17')
+
+    # what if 
+    points = flamp.to_mp([-2, -1, 0, 1, 2])
+    vals = flamp.to_mp([-1, 1, 0, 1, -1])
+    remez_error_discrete = mf.discrete_remez_error(0, vals, points, max_iter=10, tol=1e-16)
+    assert np.abs(remez_error_discrete - 1) < flamp.gmpy2.mpfr('1e-17')
+
+    points = flamp.to_mp([-3, -2, -1, 0, 1, 2, 3])
+    vals = flamp.to_mp([-1, 1, 0, 0, 0, 1, -1])
+    remez_error_discrete = mf.discrete_remez_error(0, vals, points, max_iter=10, tol=1e-16)
+    assert np.abs(remez_error_discrete - 1) < flamp.gmpy2.mpfr('1e-17')
+
+def test_perfect():
+    points = flamp.to_mp([-1, 0, 1])
+    vals = flamp.to_mp([0, 0, 0])
+    remez_error = mf.discrete_remez_error(0, vals, points, max_iter=10, tol=1e-16)
+    assert np.abs(remez_error) < flamp.gmpy2.mpfr('1e-17')
+
+
+
+# TODO add test case where, with initial (degree 0) guess, there are 4 local extrema.
+# like f(x) = (x+1)(x)(x-1) over [-2,2]
+# or sin(x)
+# also try a test case with this triangle wave and degree 0
+# np.array([0, 1, 0, 1, 0]) (((where the initial points are the first and last?)))
+# p. 301 of https://dl.acm.org/doi/pdf/10.1145/321281.321282
